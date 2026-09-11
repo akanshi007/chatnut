07643667fb156a0e5ba4de629110cfe6aa7afa54
Akanshi Sharma <A4akanshi@gmail.com>
2026-09-11 21:13:50 +0530

    Fix API_BASE port resolution and enable instant 1-click signup

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,4 @@
-const API_BASE = (window.location.protocol === 'file:' || (window.location.port && window.location.port !== '8000'))
-    ? 'http://localhost:8000'
-    : '';
+const API_BASE = window.location.protocol === 'file:' ? 'http://localhost:8000' : '';
 const socket = API_BASE ? io(API_BASE) : io();
 const params = new URLSearchParams(window.location.search);
 
